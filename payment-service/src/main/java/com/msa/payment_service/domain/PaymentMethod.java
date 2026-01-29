9f09f963737eef4ec6539f9075ecbc09dec88952
woojin <chamchi0305@naver.com>
2026-01-30 00:09:29 +0900

    chore: remove deprecated order-service, payment-service and their CI workflows

--- payment-service/src/main/java/com/msa/payment_service/domain/PaymentMethod.java
+++
@@ -1,7 +0,0 @@
-package com.msa.payment_service.domain;
-
-public enum PaymentMethod {
-    CREDIT_CARD,
-    CASH,
-    EASY_PAYMENT
-}
--- payment-service/src/main/java/com/msa/payment_service/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.msa.payment_service.repository;
-
-import com.msa.payment_service.domain.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
